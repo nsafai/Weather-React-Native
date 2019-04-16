565df606719f1d1e9f4b8bad63bef3430ebbb153
Nicolai Safai <nicolaisafai@gmail.com>
2019-04-16 14:32:47 -0700

    finished MVP of weather app

--- App.js
+++ App.js
@@ -42,7 +42,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <DisplayWeather />
+        <DisplayWeather data={this.state.weather} />
       </View>
     );
   }
--- Components/DisplayWeather.js
+++ Components/DisplayWeather.js
@@ -2,10 +2,26 @@ import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
 const DisplayWeather = (props) => {
+  // No data yet, so it must still be loading
+  if (props.data === null) {
+    return <Text>Loading...</Text>
+  }
+
+  /* Cod is where codes go, and 200 is the success code
+  So, if an error occurred, the value would not be 200 (could be 400, 404, etc.) */
+  if (props.data.cod !== 200) {
+    return <Text>An Error has occurred</Text>
+  }
+
+  // If no error and not loading, load/display weather data, grabbing the appropriate data from props
+  const { temp, temp_max, temp_min, humidity } = props.data.main
+  const { description } = props.data.weather[0]
+
   return (
       <View style={styles.container}>
-        <Text style={styles.temp}>72˚</Text>
-        <Text style={styles.description}>Mostly Sunny</Text>
+        <Text style={styles.temp}>{temp}˚</Text>
+        <Text style={styles.description}>{description}</Text>
+        <Text style={styles.temp_range}>{temp_min}˚ - {temp_max}˚</Text>
       </View>
   )
 }
@@ -23,7 +39,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  temp_range: {
+    textAlign: 'center',
+    color: '#aaa',
+  },
   description: {
+    fontSize: 15,
     textAlign: 'center',
   }
 })
